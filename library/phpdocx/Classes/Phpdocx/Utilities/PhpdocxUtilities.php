4f3a71563300b0adb280f570ae1095e030743170
Beverly Lanning <beverly.lanning@acquia.com>
2019-08-20 13:55:50 -0400

    FOIA-21: Updates the phpdocx ini file path for Acquia Cloud servers. (#67)

--- library/phpdocx/Classes/Phpdocx/Utilities/PhpdocxUtilities.php
+++ library/phpdocx/Classes/Phpdocx/Utilities/PhpdocxUtilities.php
@@ -1,87 +1,84 @@
 <?php
+
 namespace Phpdocx\Utilities;
+
 /**
- * PhpdocxUtilities
+ * PhpdocxUtilities.
  *
- * @category   Phpdocx
- * @package    utilities
- * @copyright  Copyright (c) Narcea Producciones Multimedia S.L.
+ * @category Phpdocx
+ * @package utilities
+ * @copyright Copyright (c) Narcea Producciones Multimedia S.L.
  *             (http://www.2mdc.com)
- * @license    phpdocx LICENSE
- * @version    2017.09.11
- * @link       https://www.phpdocx.com
+ * @license phpdocx LICENSE
+ * @version 2017.09.11
+ * @link https://www.phpdocx.com
  */
-class PhpdocxUtilities
-{
+class PhpdocxUtilities {
 
-    /**
-     *
-     * @access public
-     * @static
-     * @var integer
-     */
-    private static $_phpdocxConfig;
+  /**
+   *
+   * @access public
+   * @static
+   * @var int
+   */
+  private static $_phpdocxConfig;
 
-    /**
-     * Check if string is UTF8
-     * 
-     * @access public
-     * @param string $string String to check
-     * @static
-     * @return boolean
-     */
-    public static function isUtf8($string_input)
-    {
-        $string = $string_input;
+  /**
+   * Check if string is UTF8.
+   *
+   * @access public
+   * @param string $string
+   *   String to check.
+   *
+   * @static
+   *
+   * @return bool
+   */
+  public static function isUtf8($string_input) {
+    $string = $string_input;
 
-        $string = preg_replace("#[\x09\x0A\x0D\x20-\x7E]#", "", $string);
-        $string = preg_replace("#[\xC2-\xDF][\x80-\xBF]#", "", $string);
-        $string = preg_replace("#\xE0[\xA0-\xBF][\x80-\xBF]#", "", $string);
-        $string = preg_replace("#[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}#", "", $string);
-        $string = preg_replace("#\xED[\x80-\x9F][\x80-\xBF]#", "", $string);
-        $string = preg_replace("#\xF0[\x90-\xBF][\x80-\xBF]{2}#", "", $string);
-        $string = preg_replace("#[\xF1-\xF3][\x80-\xBF]{3}#", "", $string);
-        $string = preg_replace("#\xF4[\x80-\x8F][\x80-\xBF]{2}#", "", $string);
+    $string = preg_replace("#[\x09\x0A\x0D\x20-\x7E]#", "", $string);
+    $string = preg_replace("#[\xC2-\xDF][\x80-\xBF]#", "", $string);
+    $string = preg_replace("#\xE0[\xA0-\xBF][\x80-\xBF]#", "", $string);
+    $string = preg_replace("#[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}#", "", $string);
+    $string = preg_replace("#\xED[\x80-\x9F][\x80-\xBF]#", "", $string);
+    $string = preg_replace("#\xF0[\x90-\xBF][\x80-\xBF]{2}#", "", $string);
+    $string = preg_replace("#[\xF1-\xF3][\x80-\xBF]{3}#", "", $string);
+    $string = preg_replace("#\xF4[\x80-\x8F][\x80-\xBF]{2}#", "", $string);
 
-        return ($string == "" ? true : false);
+    return ($string == "" ? TRUE : FALSE);
+  }
+
+  /**
+   * Return a uniqueid to be used in tags.
+   *
+   * @access public
+   * @static
+   *
+   * @return string
+   */
+  public static function parseConfig() {
+    if (!isset(self::$_phpdocxConfig)) {
+      $phpdocxconfigfile = dirname(__FILE__) . '/../../../../../phpdocxconfig.ini';
+      if (file_exists($phpdocxconfigfile)) {
+        self::$_phpdocxConfig = parse_ini_file($phpdocxconfigfile, TRUE);
+      }
     }
+    return self::$_phpdocxConfig;
+  }
 
-    /**
-     * Return a uniqueid to be used in tags
-     *
-     * @access public
-     * @static
-     * @return string
-     */
-    public static function parseConfig()
-    {
-        if (!isset(self::$_phpdocxConfig)) {
-            if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
-              // Load secrets file, if available, see https://docs.acquia.com/resource/secrets/
-              $phpdocxconfigfile = sprintf('/mnt/files/%s.%s/phpdocxconfig.ini', $_ENV['AH_SITE_GROUP'],$_ENV['AH_SITE_ENVIRONMENT']);
-            }
-            else {
-              $phpdocxconfigfile = dirname(__FILE__) . '/../../../../../phpdocxconfig.ini';
-            }
-            if (file_exists($phpdocxconfigfile)) {
-              self::$_phpdocxConfig = parse_ini_file($phpdocxconfigfile, true);
-            }
-        }
-        return self::$_phpdocxConfig;
-    }
+  /**
+   * Return a uniqueid to be used in tags.
+   *
+   * @access public
+   * @static
+   *
+   * @return string
+   */
+  public static function uniqueId() {
+    $uniqueid = uniqid('phpdocx_');
 
-    /**
-     * Return a uniqueid to be used in tags
-     *
-     * @access public
-     * @static
-     * @return string
-     */
-    public static function uniqueId()
-    {
-        $uniqueid = uniqid('phpdocx_');
-
-        return $uniqueid;
-    }
+    return $uniqueid;
+  }
 
 }
